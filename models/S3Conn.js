8bcbfa90c76bd92f6b59291b7ae8be3df439d935
Jordan Buschman <jordan.buschman@me.com>
2015-01-26 13:14:00 -0800

    Added S3Conn.js from other file

---
+++ models/S3Conn.js
@@ -0,0 +1,125 @@
+var amazonS3 = require('awssum-amazon-s3');
+var crypto   = require('crypto');
+var fs       = require('fs');
+
+function S3Conn() {
+    var s3 = new amazonS3.S3({
+        'accessKeyId': process.env.AWSAccessKeyId,
+        'secretAccessKey' : process.env.AWSSecretKey,
+        'region': amazonS3.US_WEST_1
+    });
+
+    this.listPublicFiles = function(callback) {
+        var options = {
+            BucketName: 'jbfilespace',
+            Prefix: 'public/' 
+        };
+
+        s3.ListObjects(options, function(err, data) {
+            if (err) {
+                return callback(err);
+            }
+            else {
+                return callback(data);
+            }
+        });
+    };
+
+    this.listPrivateFiles = function(username, callback) {
+    };
+
+    this.uploadPrivateFile = function(username, password, filename, salt, hmacKey, file, callback) {
+        var options = {
+            BucketName: 'jbfilespace',
+            ObjectName: 'private/' + username + '/' + filename
+        };
+
+        crypto.pbkdf2(password, salt, 80000, 8, function(err, derivedKey) {
+            if (err)
+                return callback(err);
+
+            derivedKey = derivedKey.toString('hex');
+
+            var iv = crypto.randomBytes(8).toString('hex');
+            var cipher = crypto.createCipheriv('AES-128-CBC', derivedKey, iv);
+            var encryptedFile = cipher.update(file, 'binary', 'hex');
+            encryptedFile += cipher.final('hex');
+
+
+            var hmac = crypto.createHmac('SHA256', hmacKey);
+            hmac.setEncoding('hex');
+            hmac.end(iv.concat(encryptedFile), function() {
+                var hmacString = hmac.read();
+
+                var fileToSend = salt.concat(iv, hmacString, encryptedFile);
+                //  size(hex):    64         16     64           varies 
+
+                //Now that encryption is done, upload it to S3
+                options['ContentLength'] = fileToSend.length;
+                options['Body'] = fileToSend;
+
+                s3.PutObject(options, function(err, data) {
+                    if (err)
+                        return callback(err);
+
+                    return callback(data);            
+                });
+            });
+        });
+    };
+
+    this.getPublicFile = function(filename, callback) {
+        var options = {
+            BucketName: 'jbfilespace',
+            ObjectName: 'public/' + filename,
+        };
+
+        s3.GetObject(options, function(err, data) {
+            if (err)
+                return callback(err);
+            return callback(data);
+        });
+    };
+
+    this.getPrivateFile = function(username, password, filename, hmacKey, callback) {
+        var options = {
+            BucketName: 'jbfilespace',
+            ObjectName: 'private/' + username + '/' + filename,
+        };
+
+        s3.GetObject(options, function(err, data) {
+            if (err)
+                return callback(err);
+
+            //Now that you have the file, decrypt it
+            var file = data.Body;
+            var salt = String(file.slice(0, 64));
+            var iv = String(file.slice(64, 80));
+            var hmac = String(file.slice(80, 144));
+            var encryptedFile = String(file.slice(144, file.length));
+
+            crypto.pbkdf2(password, salt, 80000, 8, function(err, derivedKey) {
+                if (err)
+                    return callback(err);
+
+                derivedKey = derivedKey.toString('hex');
+
+                var chmac = crypto.createHmac('SHA256', hmacKey);
+                chmac.setEncoding('hex');
+                chmac.end(iv.concat(encryptedFile), function() { //Check to see if files match
+                    var hmacString = chmac.read();
+                    if (hmacString != hmac) {
+                        return callback({'StatusCode': 500, 'Body': {'Error': 'HMAC does not match.'}});
+                    }
+
+                    var decipher = crypto.createDecipheriv('AES-128-CBC', derivedKey, iv);
+                    var decrypted = Buffer.concat([decipher.update(encryptedFile, 'hex'), decipher.final()]);
+
+                    return callback({'StatusCode': 200, 'Body': decrypted });
+                });
+            });
+        });
+    };
+};
+
+module.exports = S3Conn;
