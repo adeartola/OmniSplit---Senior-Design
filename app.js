6a183e6fde2ab3b0b02ddd798a0edc2df203f505
Jordan Buschman <jordan.buschman@me.com>
2015-01-25 23:10:46 -0800

    Express setup (plus some junk from another project

---
+++ app.js
@@ -0,0 +1,86 @@
+var express      = require('express');
+var session      = require('express-session');
+var path         = require('path');
+var favicon      = require('serve-favicon');
+var logger       = require('morgan');
+var bodyParser   = require('body-parser');
+var cookieParser = require('cookie-parser');
+var flash        = require('connect-flash');
+var engine       = require('ejs-locals');
+var busboy       = require('connect-busboy');
+var passport     = require('passport');
+
+var app = express();
+var router = express.Router();
+
+/***** CONFIGURATION *****/
+require('./config/passport')(passport); // passport configuration
+
+//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser('super secret'));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+// view engine setup
+app.engine('ejs', engine);
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+// session setup
+app.use(session({
+    secret: 'super secret message',
+    resave: false,
+    saveUninitialized: false,
+}));
+
+app.use(flash());
+app.use(busboy());
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+/***** ROUTES *****/
+var index = require('./routes/index');
+//var api   = require('./routes/api');
+//var priv  = require('./routes/private');
+
+//app.use('/api', api);
+//app.use('/u', priv);
+app.use('/', index);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
+
+/***** Export and start in /bin/www *****/
+module.exports = app;
---
+++ config/passport.js
@@ -0,0 +1,33 @@
+var LocalStrategy = require('passport-local').Strategy;
+
+module.exports = function(passport) {
+    passport.serializeUser(function(user, callback) {
+        return callback(null, user);
+    });
+    passport.deserializeUser(function(id, callback) {
+        return callback(null, id);
+    });
+
+    passport.use('login', new LocalStrategy(
+        function(username, password, callback) {
+            global.db.Users.findOne({
+                where: {'username' : username },
+                attributes: ['id', 'username', 'password'],
+            }).then(function(user) {
+                if (!user) {
+                    return callback(null, false);
+                }
+                user.validatePassword(password, function(err, res) {
+                    if (err)
+                        return callback(err);
+                    else if (res) {
+                        return callback(null, [user.id, user.username, password]);
+                    }
+                    else {
+                        return callback(null, false);
+                    }
+                });
+            });
+        }
+    ));
+};
