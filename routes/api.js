a2d45bb271926d642377dbacf73df3e7cf818a24
Jordan Buschman <jordan.buschman@me.com>
2015-02-05 19:31:40 -0800

    Added /api/register (add user to database, which checks password for length and character content)

--- routes/api.js
+++ routes/api.js
@@ -8,9 +8,38 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Orderly' });
 });
 
+router.post('/register', function(req, res) {
+    //Add user to database
+    res.setHeader('Content-Type', 'application/json');
+    var email = req.body.email;
+    var password = req.body.password;
+    var regex = /(?=^.{8,}$)(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?!.*\s)[0-9a-zA-Z!@#$%^&*()]*$/;
+
+    var valid = regex.test(password);
+
+    if (valid) {
+        User.register(new User({ email: email }), password, function(err, newUser) {
+            if (err) {
+                res.status(400);
+                res.end(JSON.stringify({ error: err }) );
+            }
+            else {
+                debug('Added user ' + newUser.email + ' to users');
+                res.status(201);
+                res.end(JSON.stringify({ status: '201', message: 'Created' }) );
+            }
+        });
+    }
+    else {
+        res.status(400);
+        res.end(JSON.stringify({ error: { name: 'InvalidPasswordError', message: 'Password must have at least 8 characters, including 1 lowercase letter, 1 uppercase letter, and one digit.' } }) ); 
+    }
+});
+
 router.get('/populate', function(req, res) {
     res.render('populate', { title: 'Orderly - Populate Database' });
 }).post('/populate', function(req, res) {
+    //Reset database to default (called from GET /api/populate)
     res.setHeader('Content-Type', 'application/json');
 
     var users = [
@@ -29,13 +58,13 @@ router.get('/populate', function(req, res) {
             var completedUsers = 0;
 
             users.forEach(function(user, key) {
-                User.register(new User({ email: user.email }), user.password, function(err, tu) {
+                User.register(new User({ email: user.email }), user.password, function(err, newUser) {
                     if (err) {
                         res.status(400);
                         res.end(JSON.stringify({ error: err }) );
                     }
                     else {
-                        debug('Added ' + tu.email + ' to users');
+                        debug('Added ' + newUser.email + ' to users');
                         completedUsers++;
 
                         if (completedUsers == users.length) {
