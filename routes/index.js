916617233752581878d364436b5b7501e8492b53
Jordan <jordan.buschman@me.com>
2015-03-06 13:51:52 -0800

    Some html cleanup and added footer that I broke

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'OmniSplit' });
+    res.render('index', { title: 'Welcome to OmniSplit!' });
+});
+
+router.get('/dashboard', function(req, res) {
+    res.end(JSON.stringify({ response: 200 }) );
 });
 
 module.exports = router;
