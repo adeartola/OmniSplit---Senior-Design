bd14f307a0f0414b919d0daad8639c4ac278520e
Jordan <jordan.buschman@me.com>
2015-04-15 19:29:49 -0700

    Spinner spins when settings are being loaded

--- public/javascripts/boilerplate-dashboard.js
+++ public/javascripts/boilerplate-dashboard.js
@@ -1,5 +1,24 @@
 var omnisplitApp = angular.module('omnisplitApp', ['ngRoute']);
 
+var opts = {
+    lines: 11, // The number of lines to draw
+    length: 0, // The length of each line
+    width: 10, // The line thickness
+    radius: 20, // The radius of the inner circle
+    corners: 1, // Corner roundness (0..1)
+    rotate: 0, // The rotation offset
+    direction: 1, // 1: clockwise, -1: counterclockwise
+    color: '#000', // #rgb or #rrggbb or array of colors
+    speed: 1, // Rounds per second
+    trail: 48, // Afterglow percentage
+    shadow: false, // Whether to render a shadow
+    hwaccel: true, // Whether to use hardware acceleration
+    className: 'spinner', // The CSS class to assign to the spinner
+    zIndex: 2e9, // The z-index (defaults to 2000000000)
+    top: '50%', // Top position relative to parent
+    left: '50%' // Left position relative to parent
+};
+
 omnisplitApp.config(function($routeProvider) {
     $routeProvider
         .when('/', {
@@ -65,14 +84,20 @@ omnisplitApp.controller('settingsController', function($scope, $window) {
     };
 
     $scope.$on('$viewContentLoaded', function() {
+        var spinner;
         $.ajax({
             type: 'POST',
             url: '/api/getaddress',
             data: { id: id },
+            beforeSend: function(xhrObj){
+                var target = document.getElementById('spinner');
+                spinner = new Spinner(opts).spin(target); //Start spinner before ajax request
+            },
             success: function(data) {
                 $scope.address = angular.copy(data);
                 $scope.user = angular.copy(data);
                 $scope.$apply();
+                spinner.stop();
             }
         })
     });
@@ -123,31 +148,11 @@ omnisplitApp.directive('phoneLoaded', function() {
     };
 });
 
-var opts = {
-    lines: 11, // The number of lines to draw
-    length: 0, // The length of each line
-    width: 10, // The line thickness
-    radius: 20, // The radius of the inner circle
-    corners: 1, // Corner roundness (0..1)
-    rotate: 0, // The rotation offset
-    direction: 1, // 1: clockwise, -1: counterclockwise
-    color: '#000', // #rgb or #rrggbb or array of colors
-    speed: 1, // Rounds per second
-    trail: 48, // Afterglow percentage
-    shadow: false, // Whether to render a shadow
-    hwaccel: true, // Whether to use hardware acceleration
-    className: 'spinner', // The CSS class to assign to the spinner
-    zIndex: 2e9, // The z-index (defaults to 2000000000)
-    top: '50%', // Top position relative to parent
-    left: '50%' // Left position relative to parent
-};
-
 $(function() {
     $('#logout').on('click', function(e) {
         e.preventDefault();
         $.ajax({
             beforeSend: function(xhrObj){
-                $('body').append($('div#spinner'));
                 var target = document.getElementById('spinner');
                 spinner = new Spinner(opts).spin(target); //Start spinner before ajax request
             },
